659b61d1ad6f5cf85c26643eec9cece6687251bf
Andrés Meléndez <codeslator@gmail.com>
2024-03-16 14:34:37 -0400

    Update router;

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,43 +1,41 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Navigate, createBrowserRouter } from 'react-router-dom';
 import {
   LoginPage,
   MiddlewarePage,
 } from '../views/pages';
-import { PATHS } from '../global';
+import { PATHS, ROUTES } from '../global';
 import { AppLayout, AuthLayout } from '../views/layouts';
 
 export const router = createBrowserRouter([
   {
     path: PATHS.ROOT,
+    element: <Navigate to={ROUTES.LOGIN} />
+  },
+  {
+    path: PATHS.ROOT,
+    element: <AuthLayout />,
     children: [
       {
-        path: PATHS.ROOT,
-        element: <AuthLayout />,
+        path: PATHS.AUTH,
         children: [
           {
-            path: PATHS.AUTH,
-            children: [
-              {
-                path: PATHS.ROOT,
-                element: <MiddlewarePage />,
-              },
-              {
-                path: PATHS.LOGIN,
-                element: <LoginPage />,
-              },
-            ]
+            path: PATHS.ROOT,
+            element: <MiddlewarePage />,
+          },
+          {
+            path: PATHS.LOGIN,
+            element: <LoginPage />,
           },
         ]
       },
+    ]
+  },
 
 
-      {
-        path: PATHS.ROOT,
-        element: <AppLayout />,
-        children: [
-          
-        ]
-      },
+  {
+    path: PATHS.ROOT,
+    element: <AppLayout />,
+    children: [
 
     ]
   },
