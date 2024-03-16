e2e13f50b75d7eceeb12ec016a7ba7ed3ad98661
Ramiro Leonides Pacheco León <raleo.0486@gmail.com>
2024-03-16 12:11:38 -0700

    Create theme config and admin layouts.

--- src/views/layouts/AuthLayout.tsx
+++ src/views/layouts/AuthLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const AuthLayout: FC = () => {
   return (
-    <div className="mx-auto flex h-screen flex-col justify-center items-center bg-[#26184A]">
+    <div className="mx-auto flex h-screen flex-col justify-center items-center bg-login">
       <Outlet />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,31 @@ export default {
     'node_modules/react-daisyui/dist/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        devtalles: {
+          100: "#c5c4ff",
+          200: "#a6a1ff",
+          300: "#8d7cfd",
+          400: "#6131d1",
+          500: "#26184a",
+          600: "#1d1238",
+        }
+      }
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#6131D1",
+          "secondary": "#8D7CFD",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
