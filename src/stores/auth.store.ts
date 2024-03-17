dc001de63b0c7627f5dfe80393dcbf8586593dc2
Andrés Meléndez <codeslator@gmail.com>
2024-03-17 17:43:27 -0400

    Upload auth store changes;

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -6,37 +6,45 @@ import { User, UserDiscord } from '../interfaces/users.interface';
 export interface AuthState {
   token?: string;
   user?: User | UserDiscord;
+  isLoading: boolean;
   loginUser: (username: string, password: string) => Promise<void>;
   checkAuthStatus: () => Promise<void>;
   setUserData: (token: string, user: User | UserDiscord) => void;
+  setIsLoading: (value: boolean) => void;
   logoutUser: () => void;
 }
 
 const storeAPI: StateCreator<AuthState> = (set) => ({
   token: undefined,
   user: undefined,
+  isLoading: false,
   loginUser: async (username: string, password: string) => {
+    set({ isLoading: true })
     try {
       const { token, user } = await AuthService.login(username, password);
-      set({ token, user });
+      set({ token, user, isLoading: false });
     } catch(error) {
-      set({ token: undefined });
+      set({ user: undefined, token: undefined, isLoading: false });
       throw 'Unauthorized';
     }
   },
   checkAuthStatus: async (): Promise<void> => {
+    set({ isLoading: true })
     try {
       const { token, user } = await AuthService.checkStatus();
-      set({ token, user })
+      set({ token, user, isLoading: false })
     } catch (error) {
-      set({ user: undefined, token: undefined });
+      set({ user: undefined, token: undefined, isLoading: false });
     }
   },
   setUserData: (token: string, user: User | UserDiscord) => {
-    set({ token, user })
+    set({ token, user, isLoading: false })
+  },
+  setIsLoading: (value: boolean) => {
+    set({ isLoading: value })
   },
   logoutUser: () => {
-    set({ user: undefined, token: undefined });
+    set({ user: undefined, token: undefined, isLoading: false });
   }
 });
 
