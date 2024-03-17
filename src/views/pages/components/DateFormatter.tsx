0fc0363974d2f0e904ac95ad2e5f247a5b4aa760
Aarón Valdez <aaronvaldez2001@gmail.com>
2024-03-17 19:33:56 -0400

    Component for date formatter using Intl API

---
+++ src/views/pages/components/DateFormatter.tsx
@@ -0,0 +1,12 @@
+interface Props {
+  date: string;
+}
+export function DateFormatter({ date }: Props) {
+  return (
+    <>
+      {new Intl.DateTimeFormat("es", { dateStyle: "medium" }).format(
+        new Date(date)
+      )}
+    </>
+  );
+}
