d6f1b59deedd7f3375a8d49ca774503fe422f93b
Aarón Valdez <aaronvaldez2001@gmail.com>
2024-03-17 19:32:47 -0400

    draws services to get draws list

---
+++ src/interfaces/draws.interface.ts
@@ -0,0 +1,23 @@
+import { ResponseWithPagination } from "./response-with-pagination.interface";
+import { UserDiscord } from "./users.interface";
+
+export type DrawStatus = "pending" | "live" | "finished" | "canceled";
+
+export interface DrawData {
+  id: string;
+  title: string;
+  description: string;
+  createdBy: string;
+  status: DrawStatus;
+  available: boolean;
+  maxParticipants: number | null;
+  numberOfWinners: number;
+  prizes: string[];
+  maxDateToJoin: string | null;
+  participants: string[] | UserDiscord[];
+  winners: string[] | UserDiscord[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type DrawsListResponse = ResponseWithPagination<DrawData>;
---
+++ src/services/draws.service.ts
@@ -0,0 +1,24 @@
+import { AxiosError } from "axios";
+import { drawsApi } from "../apis/drawsApi";
+import { DrawsListResponse } from "../interfaces";
+
+export class DrawsService {
+  static getDrawsList = async (
+    page?: number,
+    limit?: number
+  ): Promise<DrawsListResponse> => {
+    try {
+      const { data } = await drawsApi.get<DrawsListResponse>("/draws", {
+        params: { limit, page },
+      });
+      return data;
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        console.log(err.response?.data);
+        throw new Error(err.response?.data);
+      }
+      console.log(err);
+      throw new Error("Unknown error");
+    }
+  };
+}
