be3537a0a82051a201b9d98ae10ae74578a5f92d
Aarón Valdez <aaronvaldez2001@gmail.com>
2024-03-18 01:50:51 -0400

    handled infinite loop checking auth session

--- src/views/layouts/PrivateRoutesLayout.tsx
+++ src/views/layouts/PrivateRoutesLayout.tsx
@@ -25,7 +25,7 @@ const PrivateRoutesLayout: FC = () => {
   };
   useEffect(() => {
     verifySessionStatus();
-  }, [authData]);
+  }, []);
 
   if (!isAuthorized) return null;
   return <Outlet />;
