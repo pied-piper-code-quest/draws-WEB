e667256a82b019bdad283b2be46f73335bf402fd
Aarón Valdez <aaronvaldez2001@gmail.com>
2024-03-18 00:42:41 -0400

    fix merge

--- src/views/pages/index.ts
+++ src/views/pages/index.ts
@@ -1,9 +1,10 @@
-export { default as LoginPage } from "./auth/LoginPage";
-export { default as MiddlewarePage } from "./middleware/MiddlewarePage";
-export { default as DashboardPage } from "./dashboard/DashboardPage";
 
-export { default as CompetitorsPage } from "./competitors/CompetitorsPage";
-export { default as AdminsPage } from "./admins/AdminsPage";
-export { default as AdminDrawsPage } from "./draws/DrawsPage";
-export { default as UserDashboardPage } from "./user-discord/UserDashboardPage";
-export { default as DrawPage } from "./user-discord/DrawPage";
+export { default as LoginPage } from './auth/LoginPage';
+export { default as MiddlewarePage } from './middleware/MiddlewarePage';
+export { default as DashboardPage } from './dashboard/DashboardPage';
+
+export { default as CompetitorsPage } from './competitors/CompetitorsPage';
+export { default as AdminsPage } from './admins/AdminsPage';
+export { default as AdminDrawsPage } from './draws/AdminDrawsPage';
+export { default as UserDashboardPage } from './user-discord/UserDashboardPage';
+export { default as DrawPage } from './user-discord/DrawPage';
